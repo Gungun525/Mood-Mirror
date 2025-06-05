120951f9f58fc771f7fee072cb5649416d20afce
Gungun525 <gungunk322@gmail.com>
2025-06-05 12:48:09 +0530

    app.py

--- MoodMirror/app.py
+++ MoodMirror/app.py
@@ -72,6 +72,6 @@ def upload_image():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # ✅ This line is key
+    app.run(host='0.0.0.0', port=port)        # ✅ This makes it public!
 
