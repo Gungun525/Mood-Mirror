0910ca25316d2a8ee5fe72a9a794099ff5b8c01d
Gungun525 <gungunk322@gmail.com>
2025-06-05 12:39:55 +0530

    app.py

--- MoodMirror/app.py
+++ MoodMirror/app.py
@@ -72,4 +72,6 @@ def upload_image():
 if __name__ == '__main__':
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
